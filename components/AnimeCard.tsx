d0d33c49fb24feda5b48f29babb07bfab1de3d12
al-shaimon <alshaimon968@gmail.com>
2023-12-23 01:58:54 +0600

    Framer Motion with Next 14

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import { MotionDiv } from '@/components/MotionDiv';
+import Image from 'next/image';
 
 export interface AnimeProp {
   id: string;
@@ -17,9 +18,25 @@ interface Prop {
   index: number;
 }
 
-function AnimeCard({ anime }: Prop) {
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
+function AnimeCard({ anime, index }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        delay: index * 0.25,
+        ease: 'easeInOut',
+        duration: 0.5,
+      }}
+      viewport={{ amount: 0 }}
+      className="max-w-sm rounded relative w-full"
+    >
       <div className="relative w-full h-[37vh]">
         <Image
           src={`https://shikimori.one${anime.image.original}`}
@@ -30,13 +47,9 @@ function AnimeCard({ anime }: Prop) {
       </div>
       <div className="py-4 flex flex-col gap-3">
         <div className="flex justify-between items-center gap-1">
-          <h2 className="font-bold text-white text-xl line-clamp-1 w-full">
-            {anime.name}
-          </h2>
+          <h2 className="font-bold text-white text-xl line-clamp-1 w-full">{anime.name}</h2>
           <div className="py-1 px-2 bg-[#161921] rounded-sm">
-            <p className="text-white text-sm font-bold capitalize">
-              {anime.kind}
-            </p>
+            <p className="text-white text-sm font-bold capitalize">{anime.kind}</p>
           </div>
         </div>
         <div className="flex gap-4 items-center">
@@ -53,18 +66,12 @@ function AnimeCard({ anime }: Prop) {
             </p>
           </div>
           <div className="flex flex-row gap-2 items-center">
-            <Image
-              src="./star.svg"
-              alt="star"
-              width={18}
-              height={18}
-              className="object-contain"
-            />
+            <Image src="./star.svg" alt="star" width={18} height={18} className="object-contain" />
             <p className="text-base font-bold text-[#FFAD49]">{anime.score}</p>
           </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 }
 
